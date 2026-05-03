2edd81e4839c0f52e02f01b9819d2ac059286274
Oleksandr-09 <164053424+Oleksandr-09@users.noreply.github.com>
2026-05-03 14:18:14 +0300

    add new changes

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,34 +1,37 @@
 const menuBtnRef = document.querySelector("[data-menu-button]");
 const mobileMenuRef = document.querySelector("[data-menu]");
-const menuLinkRefs = document.querySelectorAll(".menu-navigation-link");
+const mobileMenuLinkRef = document.querySelectorAll(".menu-navigation-link");
 
 menuBtnRef.addEventListener("click", () => {
   window.scrollTo(0, 0);
-  const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false;
 
-  menuBtnRef.classList.toggle("is-open");
+  const expanded = menuBtnRef.getAttribute("aria-expanded" === "true") || false;
   menuBtnRef.setAttribute("aria-expanded", !expanded);
 
   document.body.classList.toggle("menu-open");
+  menuBtnRef.classList.toggle("is-open");
+
   mobileMenuRef.classList.toggle("is-open");
 });
 
-menuLinkRefs.forEach((ref) => {
+mobileMenuLinkRef.forEach((ref) => {
   ref.addEventListener("click", () => {
-    menuBtnRef.classList.remove("is-open");
     menuBtnRef.setAttribute("aria-expanded", "false");
 
     document.body.classList.remove("menu-open");
+    menuBtnRef.classList.remove("is-open");
+
     mobileMenuRef.classList.remove("is-open");
   });
 });
 
-window.addEventListener("resize", (event) => {
+window.addEventListener("resize", () => {
   if (window.innerWidth >= 1440) {
-    menuBtnRef.classList.remove("is-open");
     menuBtnRef.setAttribute("aria-expanded", "false");
 
     document.body.classList.remove("menu-open");
+    menuBtnRef.classList.remove("is-open");
+
     mobileMenuRef.classList.remove("is-open");
   }
 });
